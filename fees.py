d6db88042abb905b126d5a521758fad05fce8638
Ser-GY <107444634+Ser-GY@users.noreply.github.com>
2024-02-29 14:38:55 -0500

    Update fee.py

--- fees.py
+++ fees.py
@@ -29,7 +29,12 @@ response = requests.post(NODE_RPC_URL, auth=(NODE_RPC_USER, NODE_RPC_PASS), head
 # Check if the request was successful and print the fee estimate
 if response.status_code == 200:
     result = response.json()
-    fee_rate = result['result']['feerate'] if 'feerate' in result['result'] else 'Not available'
-    print(f"Estimated fee for the next block: {fee_rate} DOGE per KB")
+    doge_fee_rate = result['result']['feerate'] if 'feerate' in result['result'] else 'Not available'
+    
+    # Convert DOGE to Satoshis
+    satoshi_fee_rate = float(doge_fee_rate) * 10**8
+    
+    print(f"Estimated fee for the next block: {doge_fee_rate} DOGE per KB")
+    print(f"Equivalent in Satoshis per KB: {satoshi_fee_rate} sats per KB")
 else:
     print("Error calling estimatesmartfee:", response.text)
